c2e5f0a9b2e5a0c3ad3ea6b03590d4c084347a82
milaq <micha.laqua@gmail.com>
2012-11-04 15:05:45 +0100

    add cherry-picks

---
+++ cherry-picks.sh
@@ -0,0 +1,4 @@
+# mkbootimg: support pagesize 8192
+cd system/core/
+git fetch https://github.com/CyanogenMod/android_system_core ics && git cherry-pick 67ffceadeab46d4a43aadac0f574b14e844e01a5
+cd ../../
