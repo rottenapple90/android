6ea06f46050b697307df567a3c8a38d30ea9db2a
milaq <micha.laqua@gmail.com>
2012-11-04 18:09:37 +0100

    rework cherry-picks, linux line feeds

--- cherry-picks.sh
+++ cherry-picks.sh
@@ -1,4 +1,10 @@
+#!/bin/bash
+#
+# cherry-picks
+# remember to cd into and out of the directories
+#
+
 # mkbootimg: support pagesize 8192
-cd system/core/
+cd system/core
 git fetch https://github.com/CyanogenMod/android_system_core ics && git cherry-pick 67ffceadeab46d4a43aadac0f574b14e844e01a5
-cd ../../
+cd ../..
