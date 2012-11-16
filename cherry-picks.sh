6292165b8294d6a1899afd7f7e6f46bb6ada9ea1
milaq <micha.laqua@gmail.com>
2012-11-16 21:38:52 +0100

    remove em102 and pagesize cherry-pick

--- cherry-picks.sh
+++ cherry-picks.sh
@@ -3,8 +3,10 @@
 # cherry-picks
 # remember to cd into and out of the directories
 #
-
+# add like this:
+#
 # mkbootimg: support pagesize 8192
-cd system/core
-git fetch https://github.com/CyanogenMod/android_system_core ics && git cherry-pick 67ffceadeab46d4a43aadac0f574b14e844e01a5
-cd ../..
+#cd system/core
+#git fetch https://github.com/CyanogenMod/android_system_core ics && git cherry-pick 67ffceadeab46d4a43aadac0f574b14e844e01a5
+#cd ../..
+#
