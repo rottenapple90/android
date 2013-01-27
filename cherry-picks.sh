7d1a226040e5b7f217a1be50b8d31e80473f8b7a
milaq <micha.laqua@gmail.com>
2013-01-27 13:38:40 +0100

    Revert "cherry-picks: UMS/MTP switch"
    is in upstream now
    
    This reverts commit 363b5d0b8fe15423f5a92c1ef64dda080ef6639c.

--- cherry-picks.sh
+++ cherry-picks.sh
@@ -10,13 +10,3 @@
 #git fetch https://github.com/CyanogenMod/android_system_core ics && git cherry-pick 67ffceadeab46d4a43aadac0f574b14e844e01a5
 #cd ../..
 #
-
-# Add option for switching between UMS and MTP/PTP mode. (1/2)
-cd frameworks/base
-git fetch http://review.cyanogenmod.org/CyanogenMod/android_frameworks_base refs/changes/95/30895/1 && git cherry-pick FETCH_HEAD
-cd ../..
-
-# Add option for switching between UMS and MTP/PTP mode. (2/2)
-cd packages/apps/Settings
-git fetch http://review.cyanogenmod.org/CyanogenMod/android_packages_apps_Settings refs/changes/96/30896/1 && git cherry-pick FETCH_HEAD
-cd ../../..
