dbd4360513c0bfe90250e7a3df9b5452ad4c3eb9
milaq <micha.laqua@gmail.com>
2013-01-30 20:14:49 +0100

    cherry-pick: TEMP - remove button-backlight inactivity timeout

--- cherry-picks.sh
+++ cherry-picks.sh
@@ -10,3 +10,9 @@
 #git fetch https://github.com/CyanogenMod/android_system_core ics && git cherry-pick 67ffceadeab46d4a43aadac0f574b14e844e01a5
 #cd ../..
 #
+# button-backlight: remove inactivity timeout
+cd frameworks/base
+wget https://raw.github.com/milaq/android/cm-10.1/patches/30-01-12_button-backlight-remove-inactivity-timeout.patch
+git am 30-01-12_button-backlight-remove-inactivity-timeout.patch
+rm 30-01-12_button-backlight-remove-inactivity-timeout.patch
+cd ../..
