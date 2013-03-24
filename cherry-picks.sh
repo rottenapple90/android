8e55c52be9878e7b2dcbe624ca054b52c8d3b385
milaq <micha.laqua@gmail.com>
2013-03-24 12:42:24 +0100

    include kovsky patches

--- cherry-picks.sh
+++ cherry-picks.sh
@@ -19,12 +19,27 @@ git am 23-03-13_add-lenovo-a107-IdeaPad-A1.patch
 rm 23-03-13_add-lenovo-a107-IdeaPad-A1.patch
 cd ../..
 
-# Add option to configure ramdisks built with mkimage for U-Boot.
+# add htc kovsky to targets
+cd vendor/cyanogen
+wget https://raw.github.com/milaq/android/gingerbread/patches/24-03-13_add-htc-kovsky.patch
+git am 24-03-13_add-htc-kovsky.patch
+rm 24-03-13_add-htc-kovsky.patch
+cd ../..
+
+# Add option to configure ramdisks built with mkimage for U-Boot (needed for a107)
 cd build
 git fetch http://review.cyanogenmod.org/CyanogenMod/android_build refs/changes/20/11920/1 && git cherry-pick FETCH_HEAD
 cd ..
 
-# SensorService: Don't always assume magnetometer is present.
+# SensorService: Don't always assume magnetometer is present (needed for a107)
 cd frameworks/base
 git fetch http://review.cyanogenmod.org/CyanogenMod/android_frameworks_base refs/changes/56/16156/1 && git cherry-pick FETCH_HEAD
 cd ../..
+
+# Added driver_cmd function to nl80211 driver (needed for kovsky)
+cd external/wpa_supplicant_6
+wget https://raw.github.com/milaq/android/gingerbread/patches/24-03-13_nl80211_driver_cmd.patch
+git am 24-03-13_nl80211_driver_cmd.patch
+rm 24-03-13_nl80211_driver_cmd.patch
+cd ../..
+
