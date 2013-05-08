806a60aa34660cc7ab489911c28c722b4f0b7dac
milaq <micha.laqua@gmail.com>
2013-05-08 13:33:05 +0200

    rework cherry-pick logic will throw an exit status of 1 if the cherry-pick failed

--- cherry-picks.sh
+++ cherry-picks.sh
@@ -1,18 +1,33 @@
 #!/bin/bash
-#
+# ---------------------------------------------------------
 # cherry-picks
-# remember to cd into and out of the directories
+# use pushd to enter directories
 #
-# add like this:
+# add cherry-picks like this:
 #
-# mkbootimg: support pagesize 8192
-#cd system/core
-#git fetch https://github.com/CyanogenMod/android_system_core ics && git cherry-pick 67ffceadeab46d4a43aadac0f574b14e844e01a5
-#cd ../..
+# # mkbootimg: support pagesize 8192
+# pushd system/core
+# git fetch https://github.com/CyanogenMod/android_system_core ics && git cherry-pick 67ffceadeab46d4a43aadac0f574b14e844e01a5
+# check_clean
+# ---------------------------------------------------------
+
+function check_clean {
+  rm *.patch
+  if [ -d ".git/rebase-apply" ]
+  then
+    git am --abort
+    popd
+    exit 1
+  fi
+  popd
+}
+
 #
+# insert cherry-picks below
+#
+
 # button-backlight: remove inactivity timeout
-cd frameworks/base
+pushd frameworks/base
 wget https://raw.github.com/milaq/android/cm-10.1/patches/12-02-13_button-backlight-remove-inactivity-timeout.patch
 git am 12-02-13_button-backlight-remove-inactivity-timeout.patch
-rm 12-02-13_button-backlight-remove-inactivity-timeout.patch
-cd ../..
+check_clean
