f30b53500c978642754c06dd03fd19064b076b40
milaq <micha.laqua@gmail.com>
2013-05-10 16:49:21 +0200

    initial commit for leo

---
+++ cherry-picks.sh
@@ -0,0 +1,33 @@
+#!/bin/bash
+# ---------------------------------------------------------
+# cherry-picks
+# use pushd to enter directories
+#
+# add cherry-picks like this:
+#
+# # mkbootimg: support pagesize 8192
+# pushd system/core
+# git fetch https://github.com/CyanogenMod/android_system_core ics && git cherry-pick 67ffceadeab46d4a43aadac0f574b14e844e01a5
+# check_clean
+# ---------------------------------------------------------
+
+function check_clean {
+  rm *.patch
+  if [ -d ".git/rebase-apply" ]
+  then
+    git am --abort
+    popd
+    exit 1
+  fi
+  popd
+}
+
+#
+# insert cherry-picks below
+#
+
+# Revert: "SurfaceFlinger: Dont skip composition if it is incomplete."
+#pushd frameworks/base
+#wget https://raw.github.com/milaq/android/ics_htc_leo/patches/10-05-13_button-backlight-remove-inactivity-timeout.patch
+#git am 10-05-13_button-backlight-remove-inactivity-timeout.patch
+#check_clean
