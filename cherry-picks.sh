f9cf7b567b5d172ec31b9dc57a738d3dba934184
milaq <micha.laqua@gmail.com>
2013-07-28 02:06:07 +0200

    Revert "remove CALL_RING patch"
    This reverts commit e180975d7dbda35b1e253ade5de08ea59f802d1e.

--- cherry-picks.sh
+++ cherry-picks.sh
@@ -32,6 +32,12 @@ wget https://raw.github.com/milaq/android/cm-10.1/patches/08-05-13_button-backli
 git am 08-05-13_button-backlight-remove-inactivity-timeout.patch
 check_clean
 
+# telephony: Support broken RIL implementations without CALL_RING events
+pushd frameworks/opt/telephony
+wget https://raw.github.com/milaq/android/cm-10.1/patches/14-05-13_telephony-Support-broken-RIL-implementations-without.patch
+git am 14-05-13_telephony-Support-broken-RIL-implementations-without.patch
+check_clean
+
 # remove cmupdater (not needed for unofficial builds)
 pushd vendor/cm
 wget https://raw.github.com/milaq/android/cm-10.1/patches/23-05-13_remove-cmupdater-not-needed-for-unofficial-builds.patch
