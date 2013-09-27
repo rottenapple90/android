d89dde57f65482ef55694868d9cacdaaf9b9a176
milaq <micha.laqua@gmail.com>
2013-09-27 18:46:27 +0200

    update tenderloin cherry-pick (fsck)

--- tenderloin_cherry-picks.sh
+++ tenderloin_cherry-picks.sh
@@ -50,6 +50,6 @@ check_clean
 
 # recovery: we need our own fsck for tenderloin
 pushd bootable/recovery
-wget https://raw.github.com/milaq/android/cm-10.2/patches/13-09-14_we-need-our-own-fsck-for-tenderloin.patch
-git am 13-09-14_we-need-our-own-fsck-for-tenderloin.patch
+wget https://raw.github.com/milaq/android/cm-10.2/patches/13-09-27_we-need-our-own-fsck-for-tenderloin.patch
+git am 13-09-27_we-need-our-own-fsck-for-tenderloin.patch
 check_clean
