945dfc5cccacf27a52335536c292a5c6cc31bcd4
Micha LaQua <micha.laqua@gmail.com>
2014-02-01 15:47:44 +0100

    remove tenderloin tinyalsa patch  * fixed in device tree by @jcsullins

--- tenderloin_cherry-picks.sh
+++ tenderloin_cherry-picks.sh
@@ -43,15 +43,6 @@ wget https://raw.github.com/milaq/android/cm-11.0/patches/${PATCH}.patch
 git am ${PATCH}.patch
 check_clean
 
-# tinyalsa: properly support multivalued controls
-PATCH=13-11-19_tinyalsa-properly-support-multivalued-controls
-FOLDER=external/tinyalsa
-###
-pushd ${FOLDER}
-wget https://raw.github.com/milaq/android/cm-11.0/patches/${PATCH}.patch
-git am ${PATCH}.patch
-check_clean
-
 # btservice/AdaperState: handle ENABLED_READY in OffState
 PATCH=13-11-25_btservice-AdaperState-handle-ENABLED_READY
 FOLDER=packages/apps/Bluetooth
